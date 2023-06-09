f4a696e06e37d3219ad33a912b595db717f8a151
Jack Sell <98410625+jhdunc@users.noreply.github.com>
2023-06-09 14:18:43 +0200

    Quest Reward Add Item

--- Assets/QuestManager.cs
+++ Assets/QuestManager.cs
@@ -7,15 +7,20 @@ public class QuestManager : MonoBehaviour
 {
     public Toggle bottleQuest;
     public PopUpWindow popUpWindow;
+    public InventoryObject inventory;
+    
+    public ItemObject item;
 
 
     //TODO CALL THIS 
-   public void BottleQuest()
+    public void BottleQuest()
     {
         bottleQuest.isOn = true;
+        
+        inventory.AddItem(new Item(item), 1);
         popUpWindow.AddToQueue("Quest Complete! You have gotten 1 Nylon Twine!");
         //TODO ADD ITEM PLEASEEEEEE
-        
+
     }
    public void RepairOxygen()
     {
