d92d3a3fcebd5f5f3f643c95665290868ab4da4a
Jack Sell <98410625+jhdunc@users.noreply.github.com>
2023-06-12 00:44:37 +0200

    Quest UI Update

--- Assets/Inventory/Scripts/QuestUI.cs
+++ Assets/Inventory/Scripts/QuestUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestUI : MonoBehaviour
 {
@@ -8,24 +9,57 @@ public class QuestUI : MonoBehaviour
     public GameObject questScreen;
     public InventoryTabs invTabs;
     public bool menuActive;
+    public bool newQuest;
+    public int questCount;
+    public Sprite questActiveIcon;
+    public Sprite noQuestIcon;
+    private float Delay = 15f;
+
 
     void Start()
     {
         buttonQuests.SetActive(true);
+        questCount = 1;
+        newQuest = true;
     }
 
    public void Update()
     {
         
-        if(!menuActive)
-        {
-            buttonQuests.SetActive(true);
-        }
-        else
+        if(menuActive)
         {
             buttonQuests.SetActive(false);
         }
+        else
+        {
+            buttonQuests.SetActive(true);
+        }
 
+        Delay -= Time.deltaTime;
+
+        if (Delay <= 0 && newQuest)
+        {
+            transform.Find("QuestIndicator").GetComponent<Animation>().Play("QuestIconNew");
+            Delay = 3f;
+        }
+
+        //If New Quest
+        if (newQuest)
+        { 
+            transform.Find("QuestIndicator").GetComponent<Image>().sprite = questActiveIcon;
+            //plus animation
+        }
+
+        //If Quest Active but not "new"
+        if(questCount>0)
+        { 
+        transform.Find("QuestIndicator").GetComponent<Image>().sprite = questActiveIcon;
+        }
+        //if No Quest
+        if (questCount == 0)
+        {
+            transform.Find("QuestIndicator").GetComponent<Image>().sprite = questActiveIcon;
+        }
     }
 
     public void OpenQuests()
